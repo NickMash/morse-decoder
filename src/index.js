29cfab507ba825a478d58bd4666e9ec70bb5db41
Your Name <nicholasgrand@gmail.com>
2020-03-22 22:13:10 +0300

    added Morse decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,60 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
+    let array = expr.split('');
+    let letters = chunk(array, 10);
+    let morseArray = [];
+
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i].includes('*')){
+            morseArray.push(' ');
+
+        }
+        let morseSymbols = chunk(letters[i], 2);
+        for (let j = 0; j < morseSymbols.length; j++){
+            if (morseSymbols[j].join('') === "10") {
+                morseArray.push('.');
+            }
+            if (morseSymbols[j].join('') === "11") {
+                morseArray.push('-');
+            }
+        }
+        morseArray.push(' ')
+    }
+    let sss = morseArray.join('');
+    return decodeMorse(sss);
 }
 
-module.exports = {
-    decode
+
+function chunk(array, size) {
+    const chunkedArr = [];
+    let index = 0;
+    while (index < array.length) {
+        chunkedArr.push(array.slice(index, size + index));
+        index += size;
+    }
+    return chunkedArr;
 }
+
+function decodeMorse(decode){
+    let words = decode.split('  ');
+    let letters = words.map((w) => w.split(' '));
+    let decoded = [];
+
+    for(let i = 0; i < letters.length; i++){
+        decoded[i] = [];
+        for(let x = 0; x < letters[i].length; x++){
+            if(MORSE_TABLE[letters[i][x]]){
+                decoded[i].push( MORSE_TABLE[letters[i][x]] );
+            }
+        }
+    }
+    return decoded.map(arr => arr.join('')).join(' ');
+}
+
+    module.exports = {
+        decode
+    }
